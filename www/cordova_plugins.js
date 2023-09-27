5a103e8a3c45c61dc4fba30642a6307fd0a0984f
Taimoor Iqbal Mirza <v-tmirza@microsoft.com>
2023-09-27 10:55:24 +0500

    initial setup

---
+++ www/cordova_plugins.js
@@ -0,0 +1,56 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "id": "cordova-plugin-ionic-keyboard.keyboard",
+        "file": "plugins/cordova-plugin-ionic-keyboard/www/ios/keyboard.js",
+        "pluginId": "cordova-plugin-ionic-keyboard",
+        "clobbers": [
+            "window.Keyboard"
+        ]
+    },
+    {
+        "id": "cordova-plugin-ionic-webview.ios-wkwebview-exec",
+        "file": "plugins/cordova-plugin-ionic-webview/src/www/ios/ios-wkwebview-exec.js",
+        "pluginId": "cordova-plugin-ionic-webview",
+        "clobbers": [
+            "cordova.exec"
+        ]
+    },
+    {
+        "id": "cordova-plugin-splashscreen.SplashScreen",
+        "file": "plugins/cordova-plugin-splashscreen/www/splashscreen.js",
+        "pluginId": "cordova-plugin-splashscreen",
+        "clobbers": [
+            "navigator.splashscreen"
+        ]
+    },
+    {
+        "id": "cordova-plugin-inappbrowser.inappbrowser",
+        "file": "plugins/cordova-plugin-inappbrowser/www/inappbrowser.js",
+        "pluginId": "cordova-plugin-inappbrowser",
+        "clobbers": [
+            "cordova.InAppBrowser.open",
+            "window.open"
+        ]
+    },
+    {
+        "id": "cordova-plugin-file-opener2.FileOpener2",
+        "file": "plugins/cordova-plugin-file-opener2/www/plugins.FileOpener2.js",
+        "pluginId": "cordova-plugin-file-opener2",
+        "clobbers": [
+            "cordova.plugins.fileOpener2"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-ionic-keyboard": "2.2.0",
+    "cordova-plugin-ionic-webview": "1.2.1",
+    "cordova-plugin-splashscreen": "5.0.4",
+    "cordova-plugin-whitelist": "1.3.5",
+    "cordova-plugin-inappbrowser": "3.2.0",
+    "cordova-plugin-file-opener2": "2.2.1"
+};
+// BOTTOM OF METADATA
+});
